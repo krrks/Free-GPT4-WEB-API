df97e6b6afc3687f1d3f894976b5c4b691e0591f
krrks <25857593+krrks@users.noreply.github.com>
2023-06-14 17:34:18 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 if __name__ == '__main__':
     import time 
     try:
-        import src.FreeGPT4_Server
+        from src.FreeGPT4_Server import app
+        app.run("0.0.0.0", port=8080, debug=False)
     except:
         print("lanch error")
     time.sleep(1800)
