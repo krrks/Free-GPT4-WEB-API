dc6b5b9dddb935abf12bd7482bda96cb45863688
krrks <25857593+krrks@users.noreply.github.com>
2023-06-14 17:24:17 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
 if __name__ == '__main__':
     import time 
+    try:
+        import src.FreeGPT4_Server
+    except:
+        print("lanch error")
     time.sleep(1800)
-    python3 ./src/FreeGPT4_Server.py
+    #python3 ./src/FreeGPT4_Server.py
